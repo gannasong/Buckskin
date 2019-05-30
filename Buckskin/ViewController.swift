a730b2ca7677cb9fbb4cdfc326852e721c07a5b7
Miguel <gannasong@gmail.com>
2019-05-31 01:17:27 +0800

    Initialization

---
+++ Buckskin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buckskin
+//
+//  Created by SUNG HAO LIN on 2019/5/31.
+//  Copyright © 2019 GanNaSong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-BuckskinTests/Pods-BuckskinTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BuckskinTests : NSObject
+@end
+@implementation PodsDummy_Pods_BuckskinTests
+@end
